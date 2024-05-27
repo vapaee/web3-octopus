6bf66514ef5e822fb493c58fcd61f3a6371b39b4
Viterbo <viter.rod@gmail.com>
2024-05-27 18:13:03 -0300

    saving wip

--- w3o-core/src/index.ts
+++ w3o-core/src/index.ts
@@ -5,30 +5,43 @@ export { Ant } from './Ant';
 
 ## w3o-core
 
-vamos a construir juntos una biblioteca de programación que se llama web3 octopus y que sirve como framework para desarrolladores para conectarse de forma fácil con una o varias blockchains dependiendo de la configuración.
+Vamos a construir juntos una biblioteca de programación que se llama "Web3 Octopus" y que sirve como framework para desarrolladores para conectarse usando typescript de forma fácil con una o varias blockchains dependiendo de la configuración.
 
-en su núcleo esta biblioteca presenta 5 espacios bien diferenciados con clases abstractas con responsabilidades muy específicas.
+En su núcleo esta biblioteca presenta 5 espacios bien diferenciados con clases abstractas con responsabilidades muy específicas.
 
 1 - Authentication 
-el primer espacio es la de autenticación que nos permite saber si el usuario está bloqueado Cuál es su address o ID cuáles son sus datos ampliados en este caso cada blockchain sabrá Cómo expandir esta clase y finalmente como firmar una transacción Y obtener la respuesta que tendrá un formato específico en cada blockchain.
+El primer espacio es la de autenticación que nos permite saber si el usuario está loqueado, Cuál es su address o ID y cuáles son sus datos ampliados (que dependerá del tipo de blockchain).
+Además, nos permite firmar transacciones y obtener información sobre las transacciones que hemos firmado (su hash).
 
 ```
-type NetworkType = 'ethereum' | 'antelope' | 'solana';
-W3oAuthenticator {
+type W3oNetworkType = 'ethereum' | 'antelope' | 'solana';
+type W3oNetworkName = string;
+type W3oAddress = string;
+abstract W3oAuthenticator {
+    constructor(name: string, type: W3oNetworkType) {}
     getName(); string; // name id of the authenticator (must be unique)
     isLogged(): boolean;
-    getNetworkType(): NetworkType;
-    getAddress(): string // alias of self.getAccount().getAddress();
+    getNetworkType(): W3oNetworkType;
+    getAddress(): W3oAddress;
     getAccount(): W3oAccount // user account data
+    isReadOnly(): boolean // if the account is not logged. Useful for exploring any account's data
     signTransaction(trx: W3oTransaction): Promise<W3oTransactionResponse>
+    login(network:W3oNetworkName): Promise<W3oAddress>
+    autoLogin(network:W3oNetworkName, address: W3oAddress): Promise<W3oAddress>
+    setReadOnly(network: W3oNetworkName, address: W3oAddress): Promise<W3oAddress>
+    snapshot(): any
 }
 W3oTransactionResponse {
     hash: string
     wait(): Promise<W3oTransactionReceipt>
 }
-W3oTransactionReceipt {}
+W3oTransactionReceipt {} // por ahora vacío. Será especificado más tarde
 W3oAccount {
+    // This is the minimal data that we need to know about an account
+    // Each subclass will have its own data
+    constructor(public authenticator: W3oAuthenticator) {}
     getAddress(): string
+    snapshot(): any
 }
 ```
 
@@ -36,10 +49,10 @@ W3oAccount {
 El segundo de los espacio habla concretamente de la blockchain, sus datos, como la identificamos y sobretodo como obtenemos información sobre los contratos y los tokens que viven en ella. Cada subclase que extienda de W3oNetwork podrá guardar su estado interno (como APYs, precios, ratios, last block, etc) y deberá implementar una serie de métodos que permitan obtener información sobre los contratos y tokens que viven en ella.
 
 ```
-W3oNetwork {
+class W3oNetwork {
     constructor(public settings: W3oNetworkSettings) {}
     getNetworkType(): NetworkType,
-    getName(): string // name id of the network (must be unique)
+    getName(): W3oNetworkName // name id of the network (must be unique)
     getChainId(): string // long hash format as id
     getDisplayName(): string // for the user to read
     getLinks(): W3oNetworkLinks // useful links for this network
@@ -47,6 +60,13 @@ W3oNetwork {
     abstract getSystemToken(): W3oToken
     abstract getTokensList(): Promise<W3oToken[]>
     abstract updateState(): Promise<void> 
+    snapshot(): any
+}
+W3oNetworkSettings {
+    name: string
+    chainId: string
+    displayName: string
+    links: W3oNetworkLinks
 }
 W3oNetworkLinks {
     explorer: string
@@ -57,10 +77,15 @@ W3oNetworkLinks {
 }
 W3oContractManager {
     // has an internal cache and an API for the extended classes to use
-    getContract(address): W3oContract
+    addContract(address: W3oAddress, contract: W3oContract | null): void // if contract is null, it means the address is not a contract
+    getContract(address: W3oAddress): Promise<W3oContract | null>
+    getContracts(): W3oContract[] // returns all the contracts that are not null
+    getTokenContract(symbol: string): Promise<W3oContract | null>
+    snapshot(): any
 }
 W3oContract {
     getABI(): Promise<W3oContractABI>
+    snapshot(): any
 }
 W3oToken {
     isSystemToken(): boolean;
@@ -70,13 +95,14 @@ W3oToken {
     getDecimals(): number
     getUSDPrice(): number
     getContract(): W3oContract
+    snapshot(): any
 }
 W3oNetworkManager {
     registerNetwork(network: W3oNetwork): void
-    getNetwork(name: string): W3oNetwork
+    getNetwork(name: W3oNetworkName): W3oNetwork
     getNetworks(): W3oNetwork[]
     getCurrentNetwork(): W3oNetwork
-    setCurrentNetwork(name: string): void
+    setCurrentNetwork(name: W3oNetworkName): void
     updateState(): Promise<void> // this will update the state of all the networks
     snapshot(): any; // export internal state
 }
@@ -106,7 +132,7 @@ Web3Octopus<CustomModuleType> {
         return this._modules;
     }
     registerModules(modules: CustomModuleType): void {}
-    createModules(): any // creates custom structure based on the modules paths. It register all modules on 
+    createModules() // creates custom structure based on the modules paths. It register all modules on 
     snapshot(): any; // export internal state
 }
 W3oNetworkSupportFactory<TNetwork, TAuthenticator> {
@@ -114,18 +140,18 @@ W3oNetworkSupportFactory<TNetwork, TAuthenticator> {
     getNetworkType(): NetworkType,
     getNetworks(): TNetwork[],
     getAuthenticators(): TAuthenticator[],
-    getNetwork(name: string): TNetwork,                // this will return the network instance based on the name
+    getNetwork(name: W3oNetworkName): TNetwork,                // this will return the network instance based on the name
     getAuthenticator(name: string): TAuthenticator     // this will return the authenticator instance based on the name
     snapshot(): any; // export internal state
 }
 W3oSupportSettings {
     auth: W3oAuthenticatorSettings[];
     networks: W3oNetworkSettings[];
-    modules: W3oModule[];
 }
 
 
-ejemplo de uso:
+Este es un ejemplo de un archivo de configuración que inicializa al framework con soporte para dos familias de networks: Telos EVM y Telos/EOS.
+En este caso el desarrollador ha creado una estructura de módulos personalizada que incluye dos módulos para la familia de networks Telos EVM, dos módulos para la familia de networks Telos/EOS y dos módulos que parecen independientes. Como sea, es una desición arbitraria del desarrollador y puede crear la estructura de módulos que desee. La única salvedad es que debe respetar esa estructura con los paths que le ha dado a cada módulo.
 
 ```
 // import the main class from the core
@@ -166,6 +192,7 @@ import {
     EOSConfigJSON,                        // contains the mainnet native network configuration JSON
 } from '@vapaee/w3o-eos';
 
+// ---- Create the main class instance ----
 // the developer user may be creative and create his own modules schema
 interface IMyModules {
     evm: {
@@ -179,23 +206,9 @@ interface IMyModules {
     example: ExampleModule;               // extends W3oModule, user module
     foo: FooModule;                       // extends W3oModule, user module
 }
-// paths must match the keys in the IMyModules interface
-const modules: IMyModules {
-    evm: {
-        tokens: new EthereumTokensModule('evm.tokens'),
-        balances: new EthereumBalancesModule('evm.balances')
-    },
-    zero: {
-        tokens: new AntelopeTokensModule('zero.tokens'),
-        balances: new AntelopeBalancesModule('zero.balances')
-    },
-    example: new ExampleModule('example'),
-    foo: new FooModule('foo')
-}
-
 const octopus = new Web3Octopus<IMyModules>();
-octopus.registerModules(modules);
 
+// ---- Register Telos EVM support ----
 const telosEvmSupportSettings = {
     // list of supported wallets for Ethereum-like networks
     auth: [
@@ -212,6 +225,7 @@ const telosEvmSupportSettings = {
 const telosEvmSupport = new EvmSupportFactory(telosEvmSupportSettings);
 octopus.addNetworkSupport(telosEvmSupport);
 
+// ---- Register Telos/EOS support ----
 const telosEosSupportSettings = {
     // list of supported wallets for Antelope networks
     auth: [
@@ -228,8 +242,21 @@ const telosEosSupportSettings = {
 const telosEosSupport = new AntelopeSupportFactory(telosEosSupportSettings);
 octopus.addNetworkSupport(telosEosSupport);
 
-
-
+// ---- Register the modules ----
+// paths must match the keys in the IMyModules interface
+const modules: IMyModules {
+    evm: {
+        tokens: new EthereumTokensModule('evm.tokens'),
+        balances: new EthereumBalancesModule('evm.balances')
+    },
+    zero: {
+        tokens: new AntelopeTokensModule('zero.tokens'),
+        balances: new AntelopeBalancesModule('zero.balances')
+    },
+    example: new ExampleModule('example'),
+    foo: new FooModule('foo')
+}
+octopus.registerModules(modules);
 
 octopus.init();
 ```
@@ -238,7 +265,7 @@ octopus.init();
 5 - Sessions
 El quinto y último espacio es el de sesiones que nos permite implementar una de las características más destacadas de la biblioteca que es la de poder tener múltiples sesiones abiertas en paralelo en diferentes networks y autenticadores. Cada sesión tendrá un id único (<account-address>:<network-name>) y tendrá la tarea de mantener los datos que sean relevanets para esa sesión en particular. Las Sesiones además proveerán de una forma de almacenar datos extras relevantes a la sesión. Los módulos podrán almacenar datos en la sesión y recuperarlos en cualquier momento. Luego tendremos un manejador de sesiones que nos permitirá crear, eliminar y recuperar sesiones en cualquier momento, pero más importante aún, nos permitirá cambiar de sesión activa (current) en cualquier momento.
 
-Este feature no siempre será utilizado y no queremos forzar a que el desarrollador tenga en cuenta que existen múltiples sesiones, por tanto el manejador de sesiones debe contemplar un API para múltimples seciones pero también un API usar la sessión activa como la única posible.
+Este feature no siempre será utilizado y no queremos forzar a que el desarrollador tenga en cuenta que existen múltiples sesiones, por tanto el manejador de sesiones debe contemplar un API para múltimples seciones pero también un API para usar la sessión activa como la única posible.
 
 W3oSession {
     constructor(public account: W3oAccount, public network: W3oNetwork) {}
@@ -252,20 +279,15 @@ W3oSession {
 }
 
 W3oSessionManager {
-    createSession(account: W3oAccount, network: W3oNetwork): W3oSession
+    createSession(account: W3oAccount, network: W3oNetwork): W3oSession // overide if the id already exists
+    openSession(account: W3oAccount): W3oSession // open a session with the current network
     getSession(id: string): W3oSession
+    getSessions(): W3oSession[] // returns all the sessions
     deleteSession(id: string): void
     setCurrentSession(id: string): void
     getCurrentSession(): W3oSession
     snapshot(): any; // export internal state
 }
-
-
-
-
-
-
-
 
 
 
@@ -287,4 +309,5 @@ W3oModule
 W3oSessionManager
 W3oSession
 Web3Octopus
+
 */
