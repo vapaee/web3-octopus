6d090b010ded4f58d77576d9b3dcb78a0c05e877
Viterbo Rodríguez <viter.rod@gmail.com>
2025-06-16 10:10:51 -0300

    removing lib folders

--- w3o-core/lib/classes/W3oTransactionResponse.js
+++
@@ -1,9 +0,0 @@
-// w3o-core/src/classes/W3oTransactionResponse.ts
-/**
- * Represents a transaction response, including its hash and a method to await the receipt
- */
-export class W3oTransactionResponse {
-    constructor(hash) {
-        this.hash = hash;
-    }
-}
