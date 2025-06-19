e81742323ed9dc43254259aed5b00410d458155a
Viterbo Rodríguez <viter.rod@gmail.com>
2025-06-19 20:40:49 -0300

    relative links fixed

--- dev/src/environments/environment.ts
+++ dev/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     network: 'mainnet',
     apiUrl: 'https://api.dev-mainnet.com',
-    tokens_list: '/assets/tokens-antelope-telos.json'
+    tokens_list: './assets/tokens-antelope-telos.json'
 };
